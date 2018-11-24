9d41b76cfcc961da622dfbdf406bb6d4ffd9be5c
chaztikov <chaztikov@gmail.com>
2018-11-23 21:38:40 -0500

    .

--- param_pack/param_pack_ex_structs.h
+++ param_pack/param_pack_ex_structs.h
@@ -4,42 +4,33 @@
 
 
 using namespace std;
- 
+
+template <typename T, typename... Args>
+void push_back_vec(std::vector<T> &v, Args &&... args)
+{
+    (v.push_back(args), ...);
+}
+
+template <typename T, typename... Args>
+void FoldWrite(
+    std::ofstream &outfile,
+    std::string &savename,
+    Args &&... args)
+{
+    outfile.open(savename, std::ios_base::app);
+    (outfile << ... << std::forward<Args>(args)) << "\n";
+}
+
 template<typename ...Args>
 void FoldPrint(Args&&... args) {
-    (cout <<  ... <<  forward<Args>(args)) << '\n';
+    (std::cout <<  ... <<  std::forward<Args>(args)) << "\n";
 }
 
-// int main()
-// {
-//     FoldPrint("hello", ", ", 10, ", ", 90.0);
-// }
-
 template<typename T, typename... Args>
-void FoldPushBack(vector<T>& v, Args&&... args)
+void FoldPushBack(std::vector<T>& v, Args&&... args)
 {
     (v.push_back(args), ...);
 }
-// int main()
-// {
-//     vector<int> v;
-// 	FoldPushBack(v, 1, 2, 3, 4);
-	
-// 	for (auto &i : v)
-// 		cout << i << "\n";
-// }
-
-
-/*
- fold over a comma operator:
-*/
-template<typename T, typename... Args>
-void push_back_vec(std::vector<T>& v, Args&&... args)
-{
-    (v.push_back(args), ...);
-}
-
-
 
 
  
