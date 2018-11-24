e5855364451cfaf67b01550ff5680f64e6b79815
chaztikov <chaztikov@gmail.com>
2018-11-23 21:06:29 -0500

    .

--- param_pack/param_pack_ex_structs.h
+++ param_pack/param_pack_ex_structs.h
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
 
 using namespace std;
  
@@ -8,7 +10,19 @@ void FoldPrint(Args&&... args) {
     (cout <<  ... <<  forward<Args>(args)) << '\n';
 }
 
+template<typename T, typename... Args>
+void FoldPushBack(vector<T>& v, Args&&... args)
+{
+    (v.push_back(args), ...);
+}
+
 int main()
 {
     FoldPrint("hello", ", ", 10, ", ", 90.0);
+
+    vector<int> v;
+	FoldPushBack(v, 1, 2, 3, 4);
+	
+	for (auto &i : v)
+		cout << i << "\n";
 }
