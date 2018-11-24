baae2efa2330ca53668bb20759a0b49510e28d4e
chaztikov <chaztikov@gmail.com>
2018-11-23 21:38:27 -0500

    .

--- param_pack/param_pack_ex_structs.h
+++ param_pack/param_pack_ex_structs.h
@@ -10,11 +10,25 @@ void FoldPrint(Args&&... args) {
     (cout <<  ... <<  forward<Args>(args)) << '\n';
 }
 
+// int main()
+// {
+//     FoldPrint("hello", ", ", 10, ", ", 90.0);
+// }
+
 template<typename T, typename... Args>
 void FoldPushBack(vector<T>& v, Args&&... args)
 {
     (v.push_back(args), ...);
 }
+// int main()
+// {
+//     vector<int> v;
+// 	FoldPushBack(v, 1, 2, 3, 4);
+	
+// 	for (auto &i : v)
+// 		cout << i << "\n";
+// }
+
 
 /*
  fold over a comma operator:
@@ -25,13 +39,23 @@ void push_back_vec(std::vector<T>& v, Args&&... args)
     (v.push_back(args), ...);
 }
 
-int main()
-{
-    FoldPrint("hello", ", ", 10, ", ", 90.0);
 
-    vector<int> v;
-	FoldPushBack(v, 1, 2, 3, 4);
-	
-	for (auto &i : v)
-		cout << i << "\n";
+
+
+ 
+template<typename ...Args> auto sum(Args ...args) 
+{ 
+    return (args + ... + 0); 
 }
+
+template<typename ...Args> auto sum2(Args ...args) 
+{ 
+    return (args + ...);
+}
+
+// int main()
+// {
+//     cout << sum(1, 2, 3, 4, 5, 6, 7) << "\n";
+// 	cout << sum2(1, 2, 3, 4, 5, 6, 7) << "\n";
+// }
+
