654f2bce66e8e13937c8e81181bead17647aef23
chaztikov <chaztikov@gmail.com>
2018-11-25 23:57:30 -0500

    .

---
+++ transform_reduce/transform_reduce.C
@@ -0,0 +1,17 @@
+#include <vector>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <execution>
+ 
+int main()
+{
+    std::vector<double> xvalues(10007, 1.0), yvalues(10007, 1.0);
+ 
+    double result = std::transform_reduce(
+        std::execution::par,
+        xvalues.begin(), xvalues.end(),
+        yvalues.begin(), 0.0
+    );
+    std::cout << result << '\n';
+}
