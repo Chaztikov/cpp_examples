5bec06d43262fe39a0e619076fce6ba889ac2488
chaztikov <chaztikov@gmail.com>
2018-12-01 23:11:25 -0500

    .

---
+++ template/vector/std_vector_templates.h
@@ -0,0 +1,16 @@
+
+
+// C++ template to print vector container elements 
+template <typename T> 
+std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) 
+{ 
+    os << "["; 
+    for (int i = 0; i < v.size(); ++i) { 
+        os << v[i]; 
+        if (i != v.size() - 1) 
+            os << ", "; 
+    } 
+    os << "]\n"; 
+    return os; 
+} 
+
